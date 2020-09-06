62489a7aedc09e4242b7caa91e80d88a4302ce3c
Stephen Bowen <Sfbowen4@gmail.com>
2020-09-05 19:20:31 -0500

    Initial check-in of module TradeBot

---
+++ TradeBot/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.ComponentModel;
+using RestSharp;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace TradeBot
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TradeBot/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using RestSharp;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+
+namespace TradeBot
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Analytics analytics = new Analytics();
+            analytics = JsonConvert.DeserializeObject<Analytics>(analytics.Content);
+            label1.Text = analytics.ProfProb;
+            label2.Text = "Average Profit: " + analytics.Profits;
+
+            Portfolio portfolio = new Portfolio();
+            portfolio = JsonConvert.DeserializeObject<Portfolio>(portfolio.Content);
+
+            List<string> TickerList = portfolio.TickerList.ToList();
+            TickerListView.ItemsSource = TickerList;
+
+            ListHeader.Text = "Portfolio Size: " + TickerList.Count;
+        }
+
+        void Analytics_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Analytics analytics = new Analytics();
+            analytics = JsonConvert.DeserializeObject<Analytics>(analytics.Content);
+            label1.Text = analytics.ProfProb;
+            label2.Text = "Average Profit: " + analytics.Profits;
+
+            Portfolio portfolio = new Portfolio();
+            portfolio = JsonConvert.DeserializeObject<Portfolio>(portfolio.Content);
+
+            List<string> TickerList = portfolio.TickerList.ToList();
+            TickerListView.ItemsSource = TickerList;
+
+            ListHeader.Text = "Portfolio Size: " + TickerList.Count;
+        }
+
+    }
+
+    class Analytics
+    {
+        public string ProfProb { get; set; }
+        public string Profits { get; set; }
+        public string Content;
+
+        public Analytics()
+        {
+            RestClient client = new RestClient("http://24.107.73.103:8000/analytics");
+            var request = new RestRequest(Method.GET);
+            Content = client.Execute(request).Content;
+        }
+    }
+
+    class Portfolio
+    {
+        public string[] TickerList { get; set; }
+        public string Content;
+
+        public Portfolio()
+        {
+            RestClient client = new RestClient("http://24.107.73.103:8000/portfolio");
+            var request = new RestRequest(Method.GET);
+            Content = client.Execute(request).Content;
+        }
+    }
+}
